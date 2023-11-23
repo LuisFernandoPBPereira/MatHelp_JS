e737f74c4a601cd1af62279b104a84bb7c698f86
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2023-11-23 17:00:08 -0300

    Área do Triângulo Retângulo está feita

--- JS/areaTrianRetan.js
+++ JS/areaTrianRetan.js
@@ -1,3 +1,13 @@
 function areaTrianRetan(){
-    //Escreva a sua lógica
+    let base = parseFloat(document.getElementById("base").value);
+    let altura = parseFloat(document.getElementById("altura").value);
+    let resultado = document.getElementById("result");
+
+    if(isNaN(base) || isNaN(altura)){
+        resultado.innerHTML = "Digite os valores corretamente";
+    }
+    else{
+        let area = (base * altura)/2;
+        resultado.innerHTML = `Área = ${area}`;
+    }
 }
