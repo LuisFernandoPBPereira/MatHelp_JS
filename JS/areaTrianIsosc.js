ae7f4ef237153cb6362a9ca2da2d0fa1ceafaaaf
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2023-11-23 16:55:52 -0300

    Área do Triângulo Isósceles feita
    - Pequenas alterações na maioria dos arquivos HTML
    - Área do Triângulo Isósceles está funcionando

--- JS/areaTrianIsosc.js
+++ JS/areaTrianIsosc.js
@@ -1,3 +1,14 @@
 function areaTrianIsosc(){
-    //Escreva a sua lógica
+    let base = parseFloat(document.getElementById("base").value);
+    let altura = parseFloat(document.getElementById("altura").value);
+    let resultado = document.getElementById("result");
+
+    if(isNaN(base) || isNaN(altura)){
+        resultado.innerHTML = "Digite os valores corretamente."
+    }
+    else{
+        let area = (base * altura)/2;
+        resultado.innerHTML = `Área = ${area}`;
+    }
+
 }
