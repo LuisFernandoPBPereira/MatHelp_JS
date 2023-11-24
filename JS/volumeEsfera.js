7cc206bf4255d3a3714a60f497779dba1836a90a
Luana Yasmim <luanayasmim11@outlook.com>
2023-11-24 15:57:25 -0300

    Calculos de volume
    Criado os calculos de volume do cub, esfera e do paralelepípedo.

--- JS/volumeEsfera.js
+++ JS/volumeEsfera.js
@@ -3,5 +3,14 @@
 */
 
 function volumeEsfera(){
-    //Escreva a sua lógica
+    let raio = parseFloat(document.getElementById("raio").value);
+    let resultado = document.getElementById("result");
+
+    if(isNaN(raio) || raio<=0)
+        resultado.innerHTML = "Digite o valor corretamente";
+
+    else{
+        let volume = (4/3) * Math.PI * Math.pow(raio, 3);
+        resultado.innerHTML = `Volume = ${volume.toFixed(3)}`;
+    }
 }
--- JS/volumeParalele.js
+++ JS/volumeParalele.js
@@ -3,5 +3,16 @@
 */
 
 function volumeParalele(){
-    //Escreva sua lógica aqui
+    let altura = parseFloat(document.getElementById("altura").value);
+    let largura = parseFloat(document.getElementById("largura").value);
+    let comprimento = parseFloat(document.getElementById("comprimento").value);
+    let resultado = document.getElementById("result");
+
+    if(isNaN(altura) || isNaN(largura) || isNaN(comprimento))
+        resultado.innerHTML = "Digite o valor corretamente";
+
+    else{
+        let volume = altura * largura * comprimento;
+        resultado.innerHTML = `Volume = ${volume}`;
+    }
 }
