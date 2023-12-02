fd2e9512db1818daefe86cf575f6998dda191b7a
Francielly oliveira <franciellyoliveiraa06@gmail.com>
2023-12-01 23:30:08 -0300

    areaQuad, mediaArit e pitagoras

--- JS/mediaArit.js
+++ JS/mediaArit.js
@@ -3,5 +3,16 @@
 */
 
 function mediaArit(){
-    //Escreva a sua lógica
-}
+    let v1 = parseFloat (document.getElementById("valor1").value);
+    let v2 = parseFloat (document.getElementById("valor2").value);
+    let v3 = parseFloat (document.getElementById("valor3").value);
+    let resultado = document.getElementById ("result");
+
+    if (isNaN(v1) || isNaN (v2) || isNaN (v3))
+    resultado.innerHTML = "Digite o valor corretamente";
+
+    else {
+        let media = (v1 + v2 + v3)/3;
+        resultado.innerHTML = `Media = ${media.toFixed(2)}`;
+    }
+}   
--- JS/pitagoras.js
+++ JS/pitagoras.js
@@ -3,5 +3,15 @@
 */
 
 function pitagoras(){
-    //Escreva a sua lógica
+    let c1 = parseFloat (document.getElementById("cateto1").value);
+    let c2 = parseFloat (document.getElementById("cateto2").value);
+    let resultado = document.getElementById("result");
+
+    if (isNaN(c1) || isNaN (c2)) 
+    resultado.innerHTML = "Digite o valor corretamente";
+
+    else {
+        let hipotenusa = (c1 * c1) + (c2 * c2);
+        resultado.innerHTML = `Hipotenusa = ${hipotenusa}`;
+    }
 }
