209a65cdd366d7c3638ff0fc44f067205a2f2905
Luana Yasmim <luanayasmim11@outlook.com>
2023-12-04 11:11:37 -0300

    media ponderada

--- JS/mediaPond.js
+++ JS/mediaPond.js
@@ -3,16 +3,33 @@
 */
 
 function mediaPond() {
-    const { notas, pesos } = obterNotasEPesos();
-    const resultado = calcularMediaPonderada(notas, pesos);
+    const notas = [];
+    const pesos = [];
 
-    console.log("A média ponderada é:", resultado);
+    let resultado = document.getElementById("result");
+
+    //Buscando valores dos pesos e das notas
+    for (let i = 1; i < 4; i++) {
+        let nota = parseFloat(document.getElementById(`valor${i}`).value);
+        let peso = parseFloat(document.getElementById(`peso${i}`).value);
+
+        if (isNaN(nota) || isNaN(peso)) {
+            resultado.innerHTML = "Preencha todos os campos";
+            return;
+        }
+        else {
+            notas.push(nota);
+            pesos.push(peso);
+        }
+    }
+
+    //Calculando média ponderada
+    let mediaPonderada = calcularMediaPonderada(notas, pesos);
+    resultado.innerHTML = `Média = ${mediaPonderada}`;
+
 }
 
 function calcularMediaPonderada(notas, pesos) {
-    if (notas.length !== pesos.length) {
-        throw new Error("O número de notas deve ser igual ao número de pesos.");
-    }
 
     const somaProdutos = notas.reduce((acumulador, nota, indice) => {
         return acumulador + nota * pesos[indice];
@@ -24,21 +41,4 @@ function calcularMediaPonderada(notas, pesos) {
 
     const mediaPonderada = somaProdutos / somaPesos;
     return mediaPonderada;
-}
-
-function obterNotasEPesos() {
-    const notas = [];
-    const pesos = [];
-
-    const numeroDeNotas = parseInt(prompt("Quantas notas você tem?"));
-
-    for (let i = 0; i < numeroDeNotas; i++) {
-        const nota = parseFloat(prompt(`Digite a nota ${i + 1}:`));
-        const peso = parseFloat(prompt(`Digite o peso para a nota ${i + 1}:`));
-
-        notas.push(nota);
-        pesos.push(peso);
-    }
-
-    return { notas, pesos };
 }
