532123fba5676230b83990178c2ddd10fba76ed4
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2023-12-06 00:53:31 -0300

    HotFIx Bhaskara

--- JS/bhaskara.js
+++ JS/bhaskara.js
@@ -2,40 +2,49 @@
     Thainá
 */
 
-function calcularRaizes(a, b, c) {
-   
-    const delta = b * b - 4 * a * c;
+function bhaskara(){
+    //Coleto os valores declarados nos inputs e transformo em float
+    let axValue = parseFloat(document.getElementById("axValue").value)
+    let bxValue = parseFloat(document.getElementById("bxValue").value)
+    let cValue = parseFloat(document.getElementById("cValue").value)
 
-   
-    if (delta > 0) {
-        const x1 = (-b + Math.sqrt(delta)) / (2 * a);
-        const x2 = (-b - Math.sqrt(delta)) / (2 * a);
-        return [x1, x2];
-    } else if (delta === 0) {
-        const x = -b / (2 * a);
-        return [x];
-    } else {
-        return [];
+    //result recebe o elemento html para mostrar o resultado
+    let result = document.getElementById("result")
+    let equacao = document.getElementById("equacao")
+    
+    if(bxValue < 0){
+        equacao.innerHTML = `${axValue}X²  ${bxValue} + ${cValue} = 0`
     }
-}
-
-
-function bhaskara() {
-   
-    const a = parseFloat(document.getElementById("axValue").value);//a
-    const b = parseFloat(document.getElementById("bxValue").value);//b
-    const c = parseFloat(document.getElementById("cValue").value);//c
-
-    let resultado = document.getElementById("result");
-   
-    const raizes = calcularRaizes(a, b, c);
-
-   
-    if (raizes.length === 0) {
-        resultado.innerHTML = ("A equação não possui raízes reais.");
-    } else if (raizes.length === 1) {
-        resultado.innerHTML = (`A equação possui uma raiz real: x = ${raizes[0]}`);
-    } else {
-        resultado.innerHTML = (`A equação possui duas raízes reais: x1 = ${raizes[0]}, x2 = ${raizes[1]}`);
+    else if(cValue < 0){
+        equacao.innerHTML = `${axValue}X² + ${bxValue}  ${cValue} = 0`
     }
+    else if(bxValue < 0 && cValue < 0){
+        equacao.innerHTML = `${axValue}X²  ${bxValue}  ${cValue} = 0`
+    }
+    else{
+        equacao.innerHTML = `${axValue}X² + ${bxValue} + ${cValue} = 0`
+    }
+
+    //Faço os cálculos necessários
+    //Delta: b² - 4.a.c
+    let delta = (bxValue ** 2) - 4 * axValue * cValue 
+    //Xv: -b / 2.a
+    let verticeX = -bxValue / (2*axValue)
+    //Yv: -Δ / 4.a
+    let verticeY = -delta / (4*axValue)
+    if(delta < 0){
+        result.innerHTML = "Delta é negativo, não há raízes"
+    }
+    else{
+        //Bhaskara: -b +- √Δ / 2.a
+        let x1 = (-bxValue + Math.sqrt(delta)) / 2 * axValue
+        let x2 = (-bxValue - Math.sqrt(delta)) / 2 * axValue
+    
+        result.innerHTML = `Resultado X': ${x1} <br> 
+                            Resultado X": ${x2} <br>
+                            Delta: ${delta} <br>
+                            Vértice Xv: ${verticeX}<br>
+                            Vértice Yv: ${verticeY}`
+    }
+
 }
